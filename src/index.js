f749f79b72bbe1fa928754002dacadabc26245fc
Reema Sahu <reemasahusbp5345@gmail.com>
2024-05-10 20:53:29 -0700

    models for user, video added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import dotenv from "dotenv"
 import ConnectDB from "./db/index.js";
+import app from "./app.js";
  
 dotenv.config({
     path: './.env'
 })
-
-ConnectDB();
+const PORT=process.env.PORT || 8000;
+ConnectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Server is running at port ${PORT}`)
+    })
+}).catch((err)=>{
+    console.log("App connection failed",err)
+});
